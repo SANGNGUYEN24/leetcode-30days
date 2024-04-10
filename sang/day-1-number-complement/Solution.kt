2b030fb135ef8f4e4fa49e07828c85542c845f60
sangnd5 <sangnd5@vng.com.vn>
2024-04-10 11:48:04 +0700

    Add day 1

---
+++ sang/day-1-number-complement/Solution.kt
@@ -0,0 +1,35 @@
+class Solution {
+    fun findComplement(num: Int): Int {
+        return converBinaryToDecimal(convertDecimalToBinary(num).reversed())
+    }
+
+    fun convertDecimalToBinary(num: Int): String {
+        var dNum = num
+        var bNum = ""
+        while (dNum / 2 != 0) {
+            bNum = (dNum % 2).toString() + bNum
+            dNum = dNum / 2
+        }
+        bNum = (dNum % 2).toString() + bNum
+        return bNum
+    }
+
+    fun converBinaryToDecimal(bNum: String): Int {
+        var complement = StringBuilder()
+        var dNum = 0.0
+        for (char in bNum) {
+            val newChar = if (char == '1') '0' else '1'
+            complement.append(newChar)
+        }
+        val complementStr = complement.toString()
+
+        for (i in (complementStr.length - 1) downTo 0) {
+            val digit = complementStr.get(i)
+            if (digit == '0'){
+                continue
+            }
+            dNum += Math.pow(2.0, i.toDouble())
+        }
+        return dNum.toInt()
+    }
+}
